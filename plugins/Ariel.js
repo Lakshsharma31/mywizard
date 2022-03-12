0ccbb03c8263bbee5531022c1bee0a5591b0620d
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-03-12 19:57:32 +0530

    Update Ariel.js

--- plugins/Ariel.js
+++ plugins/Ariel.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Dont Tag My Master...
+baka!!! Dont Tag My Master...
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@919744196187/i
+handler.customPrefix = /@919389379221/i
 handler.command = new RegExp
 
 module.exports = handler
