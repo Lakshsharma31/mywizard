d8e0bc4a3cfec9f2ee460ddd2a5f656cd45185f7
Lakshsharma31 <100475528+Lakshsharma31@users.noreply.github.com>
2022-04-03 18:25:19 +0530

    Hehe

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,8 @@ function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
   this.sendContact(m.chat, '919389379221', 'ᴹᴿ᭄ ᴋᴀᴋᴀsʜɪོ ×፝֟͜× ʜᴀᴛᴀᴋᴇ', m)
-  this.sendContact(m.chat, '918266040013', '❤ᴍʀs ᴅɪᴀ ʜᴀᴛᴀᴋᴇ ×፝֟͜×❄', m)
+  this.sendContact(m.chat, '918266040013', '❤ᴍʀs ᴅɪᴀ ʜᴀᴛᴀᴋᴇ ×፝֟͜×❄', m) 
+  this.sendContact(m.chat, '919106196230', '💜Master Vminkook💜', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
